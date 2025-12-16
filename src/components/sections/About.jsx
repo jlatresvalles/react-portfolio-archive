d157a42148531e6e91665f0b5ebc6c2097b47214
jlatresvalles <jlatresvalles@gmail.com>
2025-12-16 14:46:14 -0500

    Added front-end and back-end skills

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -34,8 +34,33 @@ export const About = () => {
 
                 <div className="grid grid-cols-1 md:grid-cols-2 ga-6">
                     <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                        <h3 className="text-xl font-bold mb-4">Digital Transformation</h3>
-                        <div className="flex flex-wrap gap-2"></div>
+                        <h3 className="text-xl font-bold mb-4">Front-End Skills</h3>
+                        <div className="flex flex-wrap gap-2">
+                            {frontendSkills.map((tech, key) => (
+                                <span 
+                                    key={key}
+                                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                                hover:shadow-[0_2px_8x_rgba(59,130,246,0.2)] transition
+                                ">
+                                    {tech}
+                                </span>
+                            ))}
+                        </div>
+                    </div>
+
+                    <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                        <h3 className="text-xl font-bold mb-4">Back-End Skills</h3>
+                        <div className="flex flex-wrap gap-2">
+                            {backendSkills.map((tech, key) => (
+                                <span 
+                                    key={key}
+                                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                                hover:shadow-[0_2px_8x_rgba(59,130,246,0.2)] transition
+                                ">
+                                    {tech}
+                                </span>
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
